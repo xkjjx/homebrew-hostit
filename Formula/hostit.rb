9df9969d00aaaacff1d846b7e4775de48eddc459
Kevin Johnson <john2003.kjj@gmail.com>
2025-08-10 19:40:49 -0500

    update for version 0.0.2

--- Formula/hostit.rb
+++ Formula/hostit.rb
@@ -3,9 +3,10 @@
 class Hostit < Formula
   desc 'easily host files through the CLI'
   homepage 'https://hostit.kjonasj.com'
-  url 'https://github.com/xkjjx/hostit/archive/refs/tags/v0.0.1.tar.gz'
-  sha256 '5f026ea12129ea66ed8d678c76aa2c419102b32bee58fcd9817d831ecf9b6a5c'
+  url 'https://github.com/xkjjx/hostit/archive/refs/tags/v0.0.2.tar.gz'
+  sha256 'c07e4afc095a0301e6f1d7265794355e5ee908fc907f66c97aeda7b5bda7ce71'
   license 'MIT'
+  version "0.0.2"
 
   depends_on 'go' => :build
 
